5e585301e4b2eee350cc8f3ed9852e45b6bb22f8
crippled-ankle <pengchenuw@gmail.com>
2020-09-10 20:51:57 -0400

    Added profiler and uploaded profiling stat

--- comminimizer/src/App.js
+++ comminimizer/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './App.css';
 
@@ -175,6 +175,18 @@ class Search extends React.Component {
     });
   }
   
+  showProfilingStat(
+    id, // the "id" prop of the Profiler tree that has just committed
+    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render the entire subtree without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions // the Set of interactions belonging to this update
+  ) {
+    console.log( "The component", id, ", The phase", phase,", Time taken for the update", actualDuration, baseDuration, startTime, commitTime, interactions);
+  }
+  
   render() {
     return (
       <ReactCSSTransitionGroup 
@@ -189,57 +201,63 @@ class Search extends React.Component {
   renderPage() {
     switch(this.state.currentStep){
       case 1:
-        return (<div className="search" key="search">
-                  <div className="search-bar" key="search-bar">
-                    <input type="text" placeholder="Start by typing in the instrument name" onChange={this.onChange}></input>
-                  </div>
-                  <div className="search-result" key="search-result">
-                    { this.state.instrs.hasOwnProperty("bestMatches") &&
-                      this.state.instrs.bestMatches.map((instr, i) => {
-                        return <a key={i} href="#" onClick={(e) => this.handleClickInstr(e, instr)}>{instr["1. symbol"]} {instr["2. name"]}</a>
-                    })}
+        return (<Profiler id="Search-Profiler" onRender={this.showProfilingStat}>
+                  <div className="search" key="search">
+                    <div className="search-bar" key="search-bar">
+                      <input type="text" placeholder="Start by typing in the instrument name" onChange={this.onChange}></input>
                     </div>
-                </div>)
+                    <div className="search-result" key="search-result">
+                      { this.state.instrs.hasOwnProperty("bestMatches") &&
+                        this.state.instrs.bestMatches.map((instr, i) => {
+                          return <a key={i} href="#" onClick={(e) => this.handleClickInstr(e, instr)}>{instr["1. symbol"]} {instr["2. name"]}</a>
+                      })}
+                    </div>
+                  </div>
+                </Profiler>)
       case 2:
-        return (<div className="question-wrapper" key="question-wrapper">
-                  <div className="go-back-button-1">
-                   <svg viewBox="0 0 24 24" onClick={(e) => this.stepGoBack()}>
-                     <path d="M 20 11 H 7 l 5.5 -5.5 L 12 4 l -8 8 l 8 8 l 1 -1 L 7 13 H 20 v -2 Z"></path>
-                   </svg>
+        return (<Profiler id="Question-Profiler" onRender={this.showProfilingStat}>
+                  <div className="question-wrapper" key="question-wrapper">
+                    <div className="go-back-button-1">
+                     <svg viewBox="0 0 24 24" onClick={(e) => this.stepGoBack()}>
+                       <path d="M 20 11 H 7 l 5.5 -5.5 L 12 4 l -8 8 l 8 8 l 1 -1 L 7 13 H 20 v -2 Z"></path>
+                     </svg>
+                    </div>
+                    <div className="quantity-question" key="quantity-question">
+                      <p>Quantity?</p>
+                      <input id="quantity-unit-shares" name="quantity" type="Radio" defaultChecked/>
+                      <label onClick={(e) => this.handleClickQuantity("shares", "quantity-unit-shares")}>By Number of Units</label>
+                      <input id="quantity-unit-dollar" name="quantity" type="Radio"/>
+                      <label onClick={(e) => this.handleClickQuantity("dollar", "quantity-unit-dollar")}>By Dollar Amount</label><br />
+                      <input className="quantity-input" type="number" min="1" max="1000000" id="quantity" onChange={(e) => this.handleQuantityChange(e)} required/>
+                      <label id="unit"> {this.state.quantityType}</label><br />
+                    </div>
+                    <div className="account-question" key="account-question">
+                      <p>Account Type?</p>
+                      <input id="account-type-regular" name="account-type" type="Radio" />
+                      <label onClick={(e) => this.handleClickAcctType("regular", "account-type-regular")}>Regular Account Only</label>
+                      <input id="account-type-registered" name="account-type" type="Radio"/>
+                      <label onClick={(e) => this.handleClickAcctType("registered", "account-type-registered")}>Registered Account Only</label>
+                      <input id="account-type-all" name="account-type" type="Radio" defaultChecked/>
+                      <label onClick={(e) => this.handleClickAcctType("all", "account-type-all")}>All Account Types</label><br />
+                      <button type="button" onClick={(e) => this.handleClickCalculate()}>Calculate</button>
+                    </div>
                   </div>
-                  <div className="quantity-question" key="quantity-question">
-                    <p>Quantity?</p>
-                    <input id="quantity-unit-shares" name="quantity" type="Radio" defaultChecked/>
-                    <label onClick={(e) => this.handleClickQuantity("shares", "quantity-unit-shares")}>By Number of Units</label>
-                    <input id="quantity-unit-dollar" name="quantity" type="Radio"/>
-                    <label onClick={(e) => this.handleClickQuantity("dollar", "quantity-unit-dollar")}>By Dollar Amount</label><br />
-                    <input className="quantity-input" type="number" min="1" max="1000000" id="quantity" onChange={(e) => this.handleQuantityChange(e)} required/>
-                    <label id="unit"> {this.state.quantityType}</label><br />
-                  </div>
-                  <div className="account-question" key="account-question">
-                    <p>Account Type?</p>
-                    <input id="account-type-regular" name="account-type" type="Radio" />
-                    <label onClick={(e) => this.handleClickAcctType("regular", "account-type-regular")}>Regular Account Only</label>
-                    <input id="account-type-registered" name="account-type" type="Radio"/>
-                    <label onClick={(e) => this.handleClickAcctType("registered", "account-type-registered")}>Registered Account Only</label>
-                    <input id="account-type-all" name="account-type" type="Radio" defaultChecked/>
-                    <label onClick={(e) => this.handleClickAcctType("all", "account-type-all")}>All Account Types</label><br />
-                    <button type="button" onClick={(e) => this.handleClickCalculate()}>Calculate</button>
-                  </div>
-                </div>)
+                </Profiler>)
       case 3:
-        return (<div className="commission-panel-wrapper" key="commission-panel-wrapper">
-                  <div className="go-back-button-2">
-                    <svg viewBox="0 0 24 24" onClick={(e) => this.stepGoBack()}>
-                      <path d="M 20 11 H 7 l 5.5 -5.5 L 12 4 l -8 8 l 8 8 l 1 -1 L 7 13 H 20 v -2 Z"></path>
-                    </svg>
+        return (<Profiler id="Result-Profiler" onRender={this.showProfilingStat}>
+                    <div className="commission-panel-wrapper" key="commission-panel-wrapper">
+                    <div className="go-back-button-2">
+                      <svg viewBox="0 0 24 24" onClick={(e) => this.stepGoBack()}>
+                        <path d="M 20 11 H 7 l 5.5 -5.5 L 12 4 l -8 8 l 8 8 l 1 -1 L 7 13 H 20 v -2 Z"></path>
+                      </svg>
+                    </div>
+                    <CommissionPanel comSearchResult={this.state.comSearchResult}
+                                     quantity={this.state.quantity}
+                                     quantityType={this.state.quantityType}
+                                     instr={this.state.chosenInstr}>
+                    </CommissionPanel>
                   </div>
-                  <CommissionPanel comSearchResult={this.state.comSearchResult}
-                                   quantity={this.state.quantity}
-                                   quantityType={this.state.quantityType}
-                                   instr={this.state.chosenInstr}>
-                  </CommissionPanel>
-                </div>)
+                </Profiler>)
       default:
         return (<div className="cannot-load-page-wrapper">
                   <p>Cannot load page</p>
